8601e4453f0e1a95ba17e85d45ae0c7d84f1585d
ItzVladik <itzvladik@gmail.com>
2026-02-14 17:51:24 +0000

    Update Scripts

--- scripts/build-apk.sh
+++ scripts/build-apk.sh
@@ -3,7 +3,7 @@ git submodule init && git submodule update
 
 if [ ! -e "android-ndk-r10e" ]; then
 	wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
-	unzip android-ndk-r10e-linux-x86_64.zip
+	unzip -q android-ndk-r10e-linux-x86_64.zip
 fi
 
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
@@ -21,19 +21,19 @@ fi
 
 set -e
 
-python3 waf configure -T release --build-game=entropyzero2 --prefix=srceng-mod-launcher/android --android=aarch64,host,21 --target=../aarch64 && #--disable-warns &&
-python3 waf install --target=client,server,gamepadui,game_shader_dx9,GameUI --strip
+python3 waf configure -T release --build-game=entropyzero2 --prefix=srceng-mod-launcher/android --disable-warns --android=aarch64,host,21 --target=../aarch64 &&
+python3 waf install --target=client,server,gamepadui,game_shader_dx9,GameUI
 
 set +e
 
-export ICON=ez2.png
+export ICON=icons/ez2.png
 export PACKAGE=entropyzero2
 export APP_NAME="Entropy: Zero 2"
 
 cd srceng-mod-launcher
 
 if [ ! -e "android-sdk" ]; then
-git clone https://github.com/ItzVladik/android-sdk
+	git clone https://github.com/ItzVladik/android-sdk
 fi
 
 export ANDROID_SDK_HOME=$PWD/android-sdk
